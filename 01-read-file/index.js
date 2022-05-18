d2fb3cc68cf3f790a8badd5e1328e0fa05d8c637
Vladimir <admin@ttp.by>
2022-05-19 00:15:55 +0300

    feat: Add result at 2 task and rewrite first task

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,7 +1,15 @@
 const fs = require('fs');
 const path = require('path');
 
+/* this if other syntaxis
 fs.readFile(path.join(__dirname, 'text.txt'), 'utf-8', (err, data) => {
   if (err) throw err;
+  if (data) console.log(data);
+});
+*/
+
+const reader = new fs.ReadStream(path.join(__dirname, 'text.txt'), {encoding: 'utf-8'});
+reader.on('readable', () => {
+  let data = reader.read();
   if (data) console.log(data);
 });
---
+++ 02-write-file/index.js
@@ -0,0 +1,36 @@
+//const process = require('process');
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+const { stdin: input, stdout: output } = require('process');
+
+const writer = new fs.WriteStream(path.join(__dirname, 'out.txt'), {encoding: 'utf-8'});
+
+let rl = readline.createInterface({ input, output });
+// rl.write('Davaj poprobuju zapisat fail? ');
+
+
+// Задаем вопрос и пишем ответ в файл. При помощи метода question
+rl.question('Davaj poprobuju zapisat fail? ', (answer) => {
+  if (input === 'exit') {
+    rl.write('Ia bolshe ne budu pisat v fail, potomu chto ty napisal slovo "exit"');
+    rl.close();
+  } else {
+    writer.write(answer + '\n');
+  }
+});
+
+// Просто всё пишем в файл, проверяя не написано ли слово "exit".
+rl.on('line', (input) => {
+  if (input === 'exit') {
+    rl.write('Ia bolshe ne budu pisat v fail, potomu chto ty napisal slovo "exit"');
+    rl.close();
+  } else {
+    writer.write(input + '\n');
+  }
+});
+
+rl.on('SIGINT', () => {
+  console.log('Eto bylo zhestoko, nazhat "Ctrl + C"');
+  rl.close();
+});
